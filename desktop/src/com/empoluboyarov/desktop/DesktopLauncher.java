193b183b3aee022e4cf94bb736028640be523030
empoluboyarov <poluboyarovem@gmail.com>
2016-04-19 12:47:57 +0300

    start screen

--- desktop/src/com/empoluboyarov/desktop/DesktopLauncher.java
+++ desktop/src/com/empoluboyarov/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.empoluboyarov.FlappyBird;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		// задаем параметры для десктопной версии
+		config.width = FlappyBird.WIDTH;
+		config.height = FlappyBird.HEIGHT;
+		config.title = FlappyBird.TITLE;
+
 		new LwjglApplication(new FlappyBird(), config);
 	}
 }
